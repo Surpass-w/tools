45c7cf9b4aa453b1fc7b5ff7301445d07b6ba794
wangpeng <wangpeng@moresec.cn>
2024-06-24 19:17:10 +0800

    modify json cmd

--- internal/json.go
+++ internal/json.go
@@ -2,6 +2,7 @@ package internal
 
 import (
 	"github.com/bitly/go-simplejson"
+	"io/ioutil"
 	"os"
 )
 
@@ -18,23 +19,51 @@ var JsonOptions struct {
 }
 
 func Set(filePath string, paths []string, value interface{}) error {
-	f, err := os.Open(filePath)
+	// 打开文件，如果不存在则创建
+	f, err := os.OpenFile(filePath, os.O_RDWR|os.O_CREATE, 0644)
 	if err != nil {
-		if !os.IsNotExist(err) {
-			return err
-		}
-		f, err = os.Create(filePath)
+		return err
+	}
+	defer f.Close()
+
+	// 如果文件是新创建的，写入空的 JSON 对象
+	fileInfo, err := f.Stat()
+	if err != nil {
+		return err
+	}
+	if fileInfo.Size() == 0 {
+		_, err = f.WriteString("{}")
 		if err != nil {
 			return err
 		}
+		// 需要重新打开文件以读取内容
+		f.Close()
+		f, err = os.OpenFile(filePath, os.O_RDWR, 0644)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
 	}
-	defer f.Close()
+
+	// 解析 JSON 数据
 	j, err := simplejson.NewFromReader(f)
 	if err != nil {
 		return err
 	}
+
+	// 设置指定路径的值
 	j.SetPath(paths, value)
-	j.Encode()
+
+	// 将修改后的 JSON 数据写回文件
+	data, err := j.MarshalJSON()
+	if err != nil {
+		return err
+	}
+	err = ioutil.WriteFile(filePath, data, 0644)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
